ea25741ca118202c9bbf40378893077eb8650c9f
rahulnakre <rahulnakre@gmail.com>
2020-12-20 22:59:03 -0500

    running benchmarks on thread test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+#![feature(test)]
+
 use std::sync::{Arc, Condvar, Mutex};
 // Vector with head and tail pointer
 use std::collections::VecDeque;
 use std::thread;
+
+extern crate test;
 
 pub struct Sender<T> {
   shared: Arc<Shared<T>>,
@@ -166,6 +170,7 @@ pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
 #[cfg(test)]
 mod tests {
   use super::*;
+  use test::Bencher;
 
   #[test]
   fn ping_pong() {
@@ -200,8 +205,7 @@ mod tests {
     assert_eq!(rx.recv(), Some(10));
   }
 
-  #[test]
-  fn play2() {
+  fn play2_main() {
     let (tx, mut rx): (Sender<i32>, Receiver<i32>) = channel();  
     let mut children = Vec::new();
     static NTHREADS: i32 = 30;
@@ -229,6 +233,70 @@ mod tests {
     }
 
     println!("{:?}", ids);
+  }
+
+  #[bench]
+  fn play2(b: &mut Bencher) {
+    b.iter(|| {
+      play2_main();
+    })
+    // b.iter(|| {
+    //   let (tx, mut rx): (Sender<i32>, Receiver<i32>) = channel();  
+    //   let mut children = Vec::new();
+    //   static NTHREADS: i32 = 30;
+  
+    //   for id in 0..NTHREADS {
+    //     let mut thread_tx = tx.clone();
+  
+    //     let child = thread::spawn(move || {
+    //       thread_tx.send(id);
+  
+    //       println!("Thread {} done", id);
+    //     });
+  
+    //     children.push(child);
+    //   }
+  
+    //   // messages collected
+    //   let mut ids = Vec::with_capacity(NTHREADS as usize);
+    //   for _ in 0..NTHREADS {
+    //     ids.push(rx.recv());
+    //   }
+  
+    //   for child in children {
+    //     child.join().expect("child thread panicked");
+    //   }
+  
+    //   println!("{:?}", ids);
+  
+    // })
+    // let (tx, mut rx): (Sender<i32>, Receiver<i32>) = channel();  
+    // let mut children = Vec::new();
+    // static NTHREADS: i32 = 30;
+
+    // for id in 0..NTHREADS {
+    //   let mut thread_tx = tx.clone();
+
+    //   let child = thread::spawn(move || {
+    //     thread_tx.send(id);
+
+    //     println!("Thread {} done", id);
+    //   });
+
+    //   children.push(child);
+    // }
+
+    // // messages collected
+    // let mut ids = Vec::with_capacity(NTHREADS as usize);
+    // for _ in 0..NTHREADS {
+    //   ids.push(rx.recv());
+    // }
+
+    // for child in children {
+    //   child.join().expect("child thread panicked");
+    // }
+
+    // println!("{:?}", ids);
 
   }
 
